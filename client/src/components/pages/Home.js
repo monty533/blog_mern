15b688819b59c6d534b60a49aad73204121877d2
monty533 <monty4139@gmail.com>
2024-06-15 21:23:21 +0530

    first commit

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import LatestBlogs from "../miniComponents/LatestBlogs";
 import HeroSection from "../miniComponents/HeroSection";
 import TrendingBlogs from "../miniComponents/TrendingBlogs";
